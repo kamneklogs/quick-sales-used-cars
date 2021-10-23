9367af5f0386033054203f50adf08668f7a3a262
andreanr19 <47890681+andreanr19@users.noreply.github.com>
2021-10-23 18:41:05 -0500

    fixed partitions

--- src/Particionamiento.py
+++ src/Particionamiento.py
@@ -5,14 +5,8 @@ import numpy as np                 #Importamos la librería numpy para manipular
 import random
 from sklearn.model_selection import train_test_split
 
+
 dfencoded = pd.read_csv("data/dataset_encoded.csv")
-
-
- y = df["price"] #Variable dependiente
- x = df.drop(columns="price") #Variables independiente
-
- x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.15, random_state= 42)
-
- x_train.head()
-
- y_train.head()
+y = dfencoded["price"] #Variable dependiente
+x = dfencoded.drop(columns="price") #Variables independiente
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.15, random_state= 42)
